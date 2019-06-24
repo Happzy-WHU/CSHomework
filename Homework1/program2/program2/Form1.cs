759bb530d3268000c73cdd04943f0f0d0103870e
Your Name <you@example.com>
2019-06-24 17:29:33 +0800

    1.0

---
+++ Homework1/program2/program2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int m = Int32.Parse(textBox1.Text);
+            int n = Int32.Parse(textBox2.Text);
+            label1.Text = Convert.ToString(m*n);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
